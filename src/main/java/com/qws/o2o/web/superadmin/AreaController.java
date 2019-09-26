2f977837c470dfd0e1989c630b66769063e9f0af
wenshuai <18336136677@163.com>
2019-09-26 13:43:29 +0800

    日志文件logback的使用与说明！

--- src/main/java/com/qws/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/qws/o2o/web/superadmin/AreaController.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,15 @@ import com.qws.o2o.service.AreaService;
 public class AreaController {
 	@Autowired
 	private AreaService areaService;
+	
+	Logger logger = LoggerFactory.getLogger(AreaController.class);
 
 	@RequestMapping(value = "/listarea", method = RequestMethod.GET)
 	@ResponseBody
 	private Map<String, Object> listArea() {
+		logger.info("===start===");
+		long startTime = System.currentTimeMillis();
+
 		Map<String, Object> modelMap = new HashMap<String, Object>();
 		List<Area> list = new ArrayList<Area>();
 		try {
@@ -34,6 +41,11 @@ public class AreaController {
 			modelMap.put("success", false);
 			modelMap.put("errMsg", e.toString());
 		}
+		logger.error("test error!");
+		long endTime = System.currentTimeMillis();
+		logger.debug("costTime:[{}ms]", endTime - startTime);
+		logger.info("===end===");
+
 		return modelMap;
 	}
 
